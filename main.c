c38ca6538f4ff57e87b331459c6435f67474fec3
nacim <linnnux@hotmail.fr>
2023-11-10 09:37:14 +0000

    v 0.1.0 : add soustration function

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
+int soustration(int x, int y)
+{
+    return  x -y;
+}
+
 int main() 
 {
+
+    int resultat = 0; 
+    int x ;
+    int y ;
 
     printf("hello world\n");
     printf("bonjour à tous \n");
     printf("hola a todos \n");
 
-  
+    x = 10;
+    y = 3;
+
+    //printf("%d - %d = %d \n",x, y, soustration(x, y));
+
+    x = 100;
+    y = 86;
+    printf("%d - %d = %d \n",x, y, soustration(x, y));
+
   return 0;
 }
